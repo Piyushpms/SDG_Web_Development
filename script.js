2ce2de660ceabeb391bd60f79346582773e4ccf8
Harsh9931 <144029394+Harsh9931@users.noreply.github.com>
2024-07-17 18:36:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 
 // Example: handling the "Join our mailing list" button
 document.querySelector('.subscribe button').addEventListener('click', function() {
+    prompt("Enter your Email");
     alert('Thank you for subscribing to our newsletter!');
 });
