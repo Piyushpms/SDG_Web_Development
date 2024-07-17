cda801cf91b61d1fdf8840ee73be05f61d358321
Harsh9931 <144029394+Harsh9931@users.noreply.github.com>
2024-07-17 16:56:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//This script handles interactions and can be expanded for more functionality
+
+// Example: handling the "Join our mailing list" button
+document.querySelector('.subscribe button').addEventListener('click', function() {
+    alert('Thank you for subscribing to our newsletter!');
+});
