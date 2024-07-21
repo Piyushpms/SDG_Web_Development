d72e1323a5b10e291aeb57471fc3a04accb36aaa
Harsh9931 <hpawar2004@gmail.com>
2024-07-21 19:22:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //This script handles interactions and can be expanded for more functionality
 
 // Example: handling the "Join our mailing list" button
+
 document.querySelector('.subscribe button').addEventListener('click', function() {
-    const isLoggedIn = !!localStorage.getItem('authToken');
+    prompt("Enter your Email");
     alert('Thank you for subscribing to our newsletter!');
 });
